331299abf5d93ecfbfe75facee0111aace6a57a3
PeterSmokery <PeterSmokery@gmail.com>
2021-11-22 14:31:16 +0300

    example005 Smokery world

---
+++ example005/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("введите свой никнейм:");
+string newnickname = Console.ReadLine();
+
+if (newnickname.ToLower() == "smokery")
+{Console.WriteLine("youre welcome lord Smokery");
+}
+else {Console.Write("приветствуем тебя в обители ,");
+Console.Write(newnickname);
+}
