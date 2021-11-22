8acd470b939bf9234d4e8fec86bd964fa7304b8f
PeterSmokery <PeterSmokery@gmail.com>
2021-11-22 14:18:29 +0300

    example004 modificated with if operator and easy to check random numbers

--- example004/Program.cs
+++ example004/Program.cs
@@ -1,6 +1,8 @@
 ﻿int ura = 0;
 int numberA = new Random().Next(1,100);
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1,100);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 if (result > 100)
 {ura = 1 ;}
