f21ca3208ab462a94effaca0ab8c6a0cf831fad1
PeterSmokery <PeterSmokery@gmail.com>
2021-11-22 15:46:34 +0300

    extra_exampl001-poor dog poor friends

---
+++ extra_example001/Program.cs
@@ -0,0 +1,23 @@
+﻿int count = 0 ;
+double distance = 10000;
+double firstFriendSpeed=1;
+double secondFriendSpeed=2;
+double DogSpeed = 10;
+int friend=2;
+double time=0;
+while(distance >10)
+{
+    if(friend==1)
+    {
+    time=distance/(firstFriendSpeed+DogSpeed);
+    friend=2;
+    }
+    else
+    {
+    time=distance/(secondFriendSpeed+DogSpeed);
+    friend=1;
+    }
+distance=distance-(firstFriendSpeed+secondFriendSpeed)*time;
+count=count+1;
+}
+Console.WriteLine(count);
