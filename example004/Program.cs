52ee1995405fd63c4d93edeb0456398bd531ea41
PeterSmokery <PeterSmokery@gmail.com>
2021-11-22 14:16:05 +0300

    example004 modificated with if operator

---
+++ example004/Program.cs
@@ -0,0 +1,8 @@
+﻿int ura = 0;
+int numberA = new Random().Next(1,100);
+int numberB = new Random().Next(1,100);
+int result = numberA + numberB;
+if (result > 100)
+{ura = 1 ;}
+else {ura = 0;}
+Console.WriteLine(ura);
